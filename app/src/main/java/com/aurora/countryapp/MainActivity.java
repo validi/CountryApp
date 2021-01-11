9dbf6db9978f8d120a744eabba85174925172962
monem validi <a.validi6016@gmail.com>
2021-01-11 20:46:20 +0330

    first

--- app/src/main/java/com/aurora/countryapp/MainActivity.java
+++ app/src/main/java/com/aurora/countryapp/MainActivity.java
@@ -20,50 +20,50 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      //  setContentView(R.layout.activity_main);
+        //  setContentView(R.layout.activity_main);
 
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
     }
 
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btnSubmit:
-                String s=binding.textInputEditText2.getText().toString();
-                if(!s.equals(""))
-                {
+                String s = binding.textInputEditText2.getText().toString();
+                if (!s.equals("")) {
                     binding.txtResult.setText("loading...");
-                 getCountry(s);
+                    getCountry(s);
                 }
 
-                    break;
+                break;
         }
     }
 
-    public String getCountry(String fullname){
+    public String getCountry(String fullname) {
         final String[] result = {""};
         RetrofitClientInstance.getCountryRetrofitInstance().getCountry(fullname).enqueue(new Callback<Object>() {
             @Override
             public void onResponse(Call<Object> call, Response<Object> response) {
-               // result[0] =response.code()+"";
-String res=response.body().toString();
-                if(response.code()==200){
-                    result[0]=res.substring(2,res.length()-2).replaceAll(",","\n");
-                }else if(response.code()==404) {
-                    result[0]="Not Found";
-                }else {
+                // result[0] =response.code()+"";
+                String res = response.body().toString();
+                if (response.code() == 200) {
+                    result[0] = res.substring(2, res.length() - 2).replaceAll(",", "\n");
+                } else if (response.code() == 404) {
+                    result[0] = "Not Found";
+                } else {
 
                 }
-                 Log.i("TAG",result[0]);
-                binding.txtResult.setText(result[0]+"");
+                Log.i("TAG", result[0]);
+                binding.txtResult.setText(result[0] + "");
             }
 
             @Override
             public void onFailure(Call<Object> call, Throwable t) {
-                Log.i("TAG",t.getMessage()+"---------");
+                Log.i("TAG", t.getMessage() + "---------");
             }
         });
         return result[0];
